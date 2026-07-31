fe2d1751daff11a8bf78763d51a542ea1a11e844
alitanasokh <m.a.s.z.1.3.8.3.2000@gmail.com>
2026-08-01 03:11:37 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-let slides = document.querySelectorAll(".slide");
+
+let searchBox = document.getElementById("searchBox");
+
+if(searchBox){
+
+searchBox.addEventListener("keyup", function(){
+
+let value = searchBox.value.toLowerCase();
+
+let cards = document.querySelectorAll(".card");
+
+
+cards.forEach(function(card){
+
+let text = card.innerText.toLowerCase();
+
+if(text.includes(value)){
+card.style.display="block";
+}
+else{
+card.style.display="none";
+}
+
+});
+
+});
+
+}let slides = document.querySelectorAll(".slide");
 let index = 0;
 
 function changeSlide() {
